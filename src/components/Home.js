50e237f5f22baf6b28abd9a3f5c57eab41814f05
Rob <robert.gentile@powerhrg.com>
2021-07-01 14:11:13 -0400

    Completed Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+ const color = "firebrick"
+  return <div id="home">
+    <h1 style={{color: color}}>
+      {name} is a web developer from {city}
+    </h1>
+  </div>;
 }
 
 export default Home;
